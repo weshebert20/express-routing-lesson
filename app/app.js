e3622c3ada16397a61e4ed529b1c50304bbcf481
ZebGirouard <timzebgir@gmail.com>
2016-11-20 20:01:00 -0700

    Moving soln to other branch

--- app/app.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app     = express();
-var port    = process.env.PORT || 3000;
-var myRouter  = require('./myRouter.js');
-
-app.use('/api', myRouter);
-
-app.listen(port); 
